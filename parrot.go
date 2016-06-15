351da2405925e6a47f4d1a72a1e11da186efc60e
John Hobbs <john@velvetcache.org>
2016-06-15 10:12:55 -0500

    Add command line method to limit loops.

--- parrot.go
+++ parrot.go
@@ -2,8 +2,12 @@ package main
 
 import "github.com/nsf/termbox-go"
 import "time"
+import "flag"
 
 func main() {
+	loops := flag.Int("loops", 0, "number of times to loop (default: infinite)")
+	flag.Parse()
+
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
@@ -19,6 +23,7 @@ func main() {
 
 	termbox.SetOutputMode(termbox.Output256)
 
+	loop_index := 0
 	draw()
 
 loop:
@@ -29,6 +34,10 @@ loop:
 				break loop
 			}
 		default:
+			loop_index++
+			if *loops > 0 && (loop_index/9) >= *loops {
+				break loop
+			}
 			draw()
 			time.Sleep(75 * time.Millisecond)
 		}
