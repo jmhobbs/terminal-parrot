cd37bd8ca1dc4e9c38aabdb1a5a1c846d7395ae4
John Hobbs <john@velvetcache.org>
2018-12-13 10:49:47 -0600

    Fail if not connected to a terminal.
    Code by @Eun, pulled from jmhobbs/terminal-parrot#16

--- parrot.go
+++ parrot.go
@@ -1,14 +1,26 @@
 package main
 
-import "github.com/nsf/termbox-go"
-import "time"
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	isatty "github.com/mattn/go-isatty"
+	"github.com/nsf/termbox-go"
+)
 
 func main() {
 	loops := flag.Int("loops", 0, "number of times to loop (default: infinite)")
 	delay := flag.Int("delay", 75, "frame delay in ms")
 	orientation := flag.String("orientation", "regular", "regular or aussie")
 	flag.Parse()
+
+	if !isatty.IsTerminal(os.Stdout.Fd()) && !isatty.IsCygwinTerminal(os.Stdout.Fd()) {
+		fmt.Fprintf(os.Stderr, "%s must be run in a terminal!\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
 
 	err := termbox.Init()
 	if err != nil {
