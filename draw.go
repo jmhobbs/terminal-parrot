d72eea564c4f8c308648a79a40994cc5bb2b4d5e
John Hobbs <john@velvetcache.org>
2024-08-23 16:41:04 -0500

    Load animations from paths.

--- draw.go
+++ draw.go
@@ -8,6 +8,7 @@ import (
 )
 
 var frame_index = 0
+var color_index = 0
 
 func reverse(lines [][]byte) [][]byte {
 	slices.Reverse(lines)
@@ -24,13 +25,17 @@ func draw(animation Animation, orientation string) {
 
 	for x, line := range lines {
 		for y, cell := range line {
-			termbox.SetCell(y, x, rune(cell), colors[frame_index%len(colors)], termbox.ColorDefault)
+			termbox.SetCell(y, x, rune(cell), colors[color_index], termbox.ColorDefault)
 		}
 	}
 
 	termbox.Flush()
 	frame_index++
+	color_index++
 	if frame_index >= len(animation.Frames) {
 		frame_index = 0
+	}
+	if color_index >= len(colors) {
+		color_index = 0
 	}
 }
